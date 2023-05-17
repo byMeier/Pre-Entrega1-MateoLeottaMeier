092c238d3686ff0a7901e642fc6679de4c6ec834
Mateo Leotta Meier <mateo.leotta@gmail.com>
2023-05-17 13:45:41 -0300

    Actualización de Fetch

--- js/main.js
+++ js/main.js
@@ -1,120 +1,116 @@
 //Programa de venta de entradas, donde el precio final dependa del evento elegido, cantidad de entradas, cargos por servicio, cuantas cuotas, entre otras.
 
-//Variables de las Ventanas Emergentes traidas del HTML para el pedido de Entradas.
-const openModal = document.getElementById("openModal")
-const openModal2 = document.getElementById("openModal2")
-const openModal3 = document.getElementById("openModal3")
-const closeModal = document.getElementById("closeModal")
-const modal = document.getElementById("modal")
-//Variables de los datos ingresados por el usuario.
-let usuario = document.getElementById("usuario")
-const dni = document.getElementById("dni")
-let mail = document.getElementById("mail")
-const cantidad = document.getElementById("cantidad") //Selección de cantidad de entradas.
-const enviar = document.getElementById("enviar")
-let interes = ""
+// Variables de las Ventanas Emergentes traidas del HTML para el pedido de Entradas.
+const openModal = document.getElementById("openModal");
+const closeModal = document.getElementById("closeModal");
+const modal = document.getElementById("modal");
 
-//Funcion constructora de objetos de entradas a los conciertos.
-cargosPorServicio = 350
+// Variables de los datos ingresados por el usuario.
+let usuario = document.getElementById("usuario");
+const dni = document.getElementById("dni");
+let mail = document.getElementById("mail");
+const cantidad = document.getElementById("cantidad"); // Selección de cantidad de entradas.
+const enviar = document.getElementById("enviar");
+let interes = "";
+
+// Funcion constructora de objetos de entradas a los conciertos.
+const cargosPorServicio = 350;
 class Entrada {
-    constructor(date, importe, img, cargosPorServicio) {
-        this.date = date
-        this.importe = importe
-        this.img = img
-        this.importeConCPS = function () {
-            return this.importe + cargosPorServicio
-        }
-    }
+constructor(id, date, importe, cargosPorServicio, img) {
+    this.id = id;
+    this.date = date;
+    this.importe = importe;
+    this.cargosPorServicio = cargosPorServicio;
+    this.img = img;
 }
 
-const concerts = document.getElementById("concerts")
+importeConCPS() {
+    return this.importe + this.cargosPorServicio;
+}
+}
+
+const concerts = document.getElementById("concerts");
 const entradas = [];
 
-fetch("../assets/entradas.json") //JSON con datos de cada entrada a los conciertos.
-.then(response => response.json())
-.then(data => {
-    data.forEach(entrada => {
-        entradas.push(new Entrada(entrada.date, entrada.importe, entrada.cargosPorServicio, entrada.img))
-    })
+fetch("../assets/entradas.json") // JSON con datos de cada entrada a los conciertos.
+.then((response) => response.json())
+.then((data) => {
+    data.forEach((entrada) => {
+    entradas.push(
+        new Entrada(
+            entrada.id,
+            entrada.date,
+            entrada.importe,
+            entrada.cargosPorServicio,
+            entrada.img
+        )
+    );
+});
 
-    entradas.forEach(entrada =>{
-        openModal.innerHTML += //Ingresamos esto para que se vayan concatenando dentro del mismo contenedor y no se quede el ultimo solamente.
-            `
-            <img class="flyer" src="${entrada.img}" alt="${entrada.date}">
-            `
-    })//Y asi es como inyectamos en nuestro HTML todos los productos en nuestra base de datos más facilmente y optimizado.
+entradas.forEach((entrada) => {
+const card = document.createElement("div");
+card.innerHTML += `<img class="flyer" id="${entrada.id}" src="${entrada.img}" alt="${entrada.date}">`;
+    card.addEventListener("click", mostrarModal);
+    concerts.appendChild(card);
+    });
+});
 
-})
+function mostrarModal(e) {
+const id = parseInt(e.target.id);
+const entradaElegida = entradas.find((entrada) => entrada.id === id);
 
-/*
-//3 Tipos de entradas que se seleccionan desde el HTML.
-const entrada1 = new Entrada("Recoleta 14/01", 1000, cargosPorServicio) 
-const entrada2 = new Entrada("Musicleta 02/02", 1500, cargosPorServicio)
-const entrada3 = new Entrada("Espacio Laberinto 09/04", 2000, cargosPorServicio)
-*/
-
-
-//Todo se almacena en el LocalStorage para usarlo en la pantalla final y el usuario tenga acceso a la información.
-function guardarFormulario(){
-    //Al no estar incluidas las librerias en esta entrega aún no puedo validar estos datos, pero se hará en la entrega final.
-    //Guardamos el precio de todas las entradas en una variable y le sumamos los cargosPorServicio.
-    let valorFinal = 0;
-    for (i = 0; i < cantidad.value; i++) {
-        valorFinal += Number(localStorage.getItem("importe")) + cargosPorServicio;
-    }
-    const cuotas = parseInt(document.getElementById("cuotas").value);
-    let total;
-    switch(cuotas){ //Selección de Cuotas.
-        case 1:
-            total = valorFinal.toFixed(2);
-            interes = 0
-            break;
-        case 3:
-            total = (valorFinal / 3 * 1.1).toFixed(2);
-            interes = 10
-            break;
-        case 6:
-            total = (valorFinal / 6 * 1.15).toFixed(2);
-            interes = 15
-            break;
-    }
-    localStorage.setItem("usuario", usuario.value.trim().toUpperCase()) 
-    localStorage.setItem("dni", dni.value.trim())
-    localStorage.setItem("mail", mail.value.trim())
-    localStorage.setItem("cantidad", cantidad.value)
-    localStorage.setItem("total", total)
-    localStorage.setItem("cuotas", cuotas)
-    localStorage.setItem("CPS", cargosPorServicio)
-    localStorage.setItem("valorFinal", valorFinal)
-    localStorage.setItem("interes", interes)
+// Al elegir el concierto, ya se guarda y se define la elección y el precio del mismo.
+modal.showModal();
+    localStorage.setItem("concierto", entradaElegida.date);
+    localStorage.setItem("importe", entradaElegida.importe);
 }
 
-//Evento para cerrar las Ventana Emergentes y limpiar el Local Storage.
-closeModal.addEventListener("click",()=>{modal.close(), localStorage.clear()})
+// Todo se almacena en el LocalStorage para usarlo en la pantalla final y el usuario tenga acceso a la información.
+function guardarFormulario() {
+// Al no estar incluidas las librerías en esta entrega aún no puedo validar estos datos, pero se hará en la entrega final.
+// Guardamos el precio de todas las entradas en una variable y le sumamos los cargosPorServicio.
+let valorFinal = 0;
+for (let i = 0; i < cantidad.value; i++) {
+    valorFinal += Number(localStorage.getItem("importe")) + cargosPorServicio;
+}
 
-//Al elegir el concierto, ya se guarda y se define la elección y el precio del mismo.
-openModal.addEventListener("click",
-    function concierto1(){ 
-        modal.showModal()
-        localStorage.setItem("concierto", Entrada.date)
-        localStorage.setItem("importe", Entrada.importe)
-    })/*
-openModal2.addEventListener("click",     
-    function concierto2(){ 
-        modal.showModal()
-        localStorage.setItem("concierto", entrada2.date)
-        localStorage.setItem("importe", entrada2.importe)
-    })
-openModal3.addEventListener("click",
-    function concierto3(){ 
-        modal.showModal()
-        localStorage.setItem("concierto", entrada3.date)
-        localStorage.setItem("importe", entrada3.importe)
-    })*/
+const cuotas = parseInt(document.getElementById("cuotas").value);
+let total;
 
-    function redireccionar() {
-        window.location.href = "gracias2.html";
-        guardarFormulario()
-    }
+  switch (cuotas) { // Selección de Cuotas.
+    case 1:
+        total = valorFinal.toFixed(2);
+        interes = 0;
+        break;
+    case 3:
+        total = (valorFinal / 3 * 1.1).toFixed(2);
+        interes = 10;
+        break;
+    case 6:
+        total = (valorFinal / 6 * 1.15).toFixed(2);
+        interes = 15;
+        break;
+}
+localStorage.setItem("usuario", usuario.value.trim().toUpperCase());
+localStorage.setItem("dni", dni.value.trim());
+localStorage.setItem("mail", mail.value.trim());
+localStorage.setItem("cantidad", cantidad.value);
+localStorage.setItem("total", total);
+localStorage.setItem("cuotas", cuotas);
+localStorage.setItem("CPS", cargosPorServicio);
+localStorage.setItem("valorFinal", valorFinal);
+localStorage.setItem("interes", interes);
+}
 
-enviar.addEventListener("click", redireccionar)
+// Evento para cerrar las Ventana Emergentes y limpiar el Local Storage.
+closeModal.addEventListener("click", () => {
+    modal.close();
+    localStorage.clear();
+});
+
+function redireccionar() {
+    guardarFormulario();
+    window.location.href = "gracias2.html";
+}
+
+enviar.addEventListener("click", redireccionar);
